3ebf57456b1bb433dfdf8ac0173ab124fe17dbc7
Student <student@GAs-MacBook-Air-3.local>
2017-05-13 18:37:07 -0400

    created profile and languagelist tables

--- public/script/main.js
+++ public/script/main.js
@@ -1,17 +1,21 @@
 console.log('working hahahhahaha');
 
-(function(){
-      var pubnub = new PubNub({ publishKey : 'demo', subscribeKey : 'demo' });
-      function $(id) { return document.getElementById(id); }
-      var box = $('box'), input = $('input'), channel = '10chat-demo';
-      pubnub.addListener({
-          message: function(obj) {
-              box.innerHTML = (''+obj.message).replace( /[<>]/g, '' ) + '<br>' + box.innerHTML
-          }});
-      pubnub.subscribe({channels:[channel]});
-      input.addEventListener('keyup', function(e) {
-          if ((e.keyCode || e.charCode) === 13) {
-            pubnub.publish({channel : channel,message : input.value,x : (input.value='')});
+(function() {
+    var pubnub = new PubNub({ publishKey: 'demo', subscribeKey: 'demo' });
+
+    function $(id) { return document.getElementById(id); }
+    var box = $('box'),
+        input = $('input'),
+        channel = '10chat-demo';
+    pubnub.addListener({
+        message: function(obj) {
+            box.innerHTML = ('' + obj.message).replace(/[<>]/g, '') + '<br>' + box.innerHTML;
         }
-      });
+    });
+    pubnub.subscribe({ channels: [channel] });
+    input.addEventListener('keyup', function(e) {
+        if ((e.keyCode || e.charCode) === 13) {
+            pubnub.publish({ channel: channel, message: input.value, x: (input.value = '') });
+        }
+    });
 })();
