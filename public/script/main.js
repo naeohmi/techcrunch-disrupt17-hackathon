2eef21e50375b671bade2d52bc79416e85261934
anthony pegues <apegraduate8@gmail.com>
2017-05-14 10:12:18 -0400

    working

--- public/script/main.js
+++ public/script/main.js
@@ -95,26 +95,27 @@ submitbttn.addEventListener("click", function () {
 
 });
 
-// (function () {
-//         var pubnub = new PubNub({ publishKey: 'demo', subscribeKey: 'demo' });
-//         function $(id) { return document.getElementById(id); }
-//         var box = $('box'), input = $('#input'), channel = '10chat-demo';
-//         pubnub.addListener({
-//             message: function (obj) {
-//                 box.innerHTML = ('' + obj.message).replace(/[<>]/g, '') + '<br>' + box.innerHTML
-//             }
-//         });
-//         pubnub.subscribe({ channels: [channel] });
-//         input.addEventListener('keyup', function (e) {
+(function () {
+        var pubnub = new PubNub({ publishKey: 'demo', subscribeKey: 'demo' });
+        function $(id) { return document.getElementById(id); }
+        var box = $('box'), input = $('#input'), channel = '10chat-demo';
+        pubnub.addListener({
+            message: function (obj) {
+                    debugger
+                box.innerHTML = ('' + obj.message).replace(/[<>]/g, '') + '<br>' + box.innerHTML
+            }
+        });
+        pubnub.subscribe({ channels: [channel] });
+        input.addEventListener('keyup', function (e) {
 
-//             if ((e.keyCode || e.charCode) === 13) {
-//                 pubnub.publish({ channel: channel, message: ': ' + input.value, x: (input.value = '') });
-//             }
+            if ((e.keyCode || e.charCode) === 13) {
+                pubnub.publish({ channel: channel, message: ': ' + input.value, x: (input.value = '') });
+            }
 
-//         });
-//     }
+        });
+    }
 
-// )();
+)();
 
 
 
