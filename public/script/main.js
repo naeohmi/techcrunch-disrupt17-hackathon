c2e56b26d3488f6d43190c9d9754d41c067e555c
anthony pegues <apegraduate8@gmail.com>
2017-05-14 08:57:33 -0400

    oto

--- public/script/main.js
+++ public/script/main.js
@@ -21,9 +21,19 @@
 //     });
 // })();
 
+
+
+
+
+
+
+
+
+
+
 let grab = () => {
     $("#mod")[0].addEventListener("click", function(e) {
-        // startChat(userEmail)
+        startChat(userEmail)
         console.log(eh)
         console.log($(".modal-footer > input").val());
     });
@@ -42,10 +52,60 @@ let startChat = (user) => {
 
     pubnub.subscribe({ channels: [helperChannel] });
 
-    pubnub.publish({ channel: helperChannel, message: input.value, x: (input.value = '') });
+    pubnub.publish({
+     channel: helperChannel,
+      message: input.value,
+       x: (input.value = '') });
 };
 
-})();
+// })();
+
+
+// console.log('working hahahhahaha');
+
+// let user = '';
+
+// const submitbttn = document.getElementById('submitbttn');
+// let nameInput = document.getElementById('nameInput');
+// let enterName = document.getElementById('enterName');
+
+// submitbttn.addEventListener("click", function () {
+//     user = nameInput.value;
+//     enterName.style.display = "none";
+
+// });
+
+// (function () {
+//         var pubnub = new PubNub({ publishKey: 'demo', subscribeKey: 'demo' });
+//         function $(id) { return document.getElementById(id); }
+//         var box = $('box'), input = $('input'), channel = '10chat-demo';
+//         pubnub.addListener({
+//             message: function (obj) {
+//                 box.innerHTML = ('' + obj.message).replace(/[<>]/g, '') + '<br>' + box.innerHTML
+//             }
+//         });
+//         pubnub.subscribe({ channels: [channel] });
+//         input.addEventListener('keyup', function (e) {
+
+//             if ((e.keyCode || e.charCode) === 13) {
+//                 pubnub.publish({ channel: channel, message: user + ': ' + input.value, x: (input.value = '') });
+//             }
+
+//         });
+//     }
+
+// )();
+
+
+
+
+
+
+
+
+
+
+
 
 
 // c489eefa5f1fe70cd00ca001f09c00bb3e23c398
