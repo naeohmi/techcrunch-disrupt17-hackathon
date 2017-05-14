919035a344312e48c7025b66eaf912171dddce9f
anthony pegues <apegraduate8@gmail.com>
2017-05-13 20:19:27 -0400

    added pubsnub keys in main.js

--- public/script/main.js
+++ public/script/main.js
@@ -1,7 +1,7 @@
 console.log('working hahahhahaha');
 
 (function() {
-    var pubnub = new PubNub({ publishKey: 'demo', subscribeKey: 'demo' });
+    var pubnub = new PubNub({ publishKey: 'pub-c-c380fb6c-9e63-4585-b543-fbf2a36c5cc0', subscribeKey: 'sub-c-e2bcc08c-381a-11e7-a268-0619f8945a4f' });
 
     function $(id) { return document.getElementById(id); }
     var box = $('box'),
@@ -12,10 +12,10 @@ console.log('working hahahhahaha');
             box.innerHTML = ('' + obj.message).replace(/[<>]/g, '') + '<br>' + box.innerHTML;
         }
     });
-    pubnub.subscribe({ channels: [channel] });
+    pubnub.subscribe({ channels: [helperChannel] });
     input.addEventListener('keyup', function(e) {
         if ((e.keyCode || e.charCode) === 13) {
-            pubnub.publish({ channel: channel, message: input.value, x: (input.value = '') });
+            pubnub.publish({ channel: helperChannel, message: input.value, x: (input.value = '') });
         }
     });
 })();
